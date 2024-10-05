4e912addec6ce0b5ad9cffa1ef806b35015052ae
epkaz93 <thomaswilmot@gmail.com>
2024-10-05 16:18:20 +1000

    Rearranged ToggleTerm configuration

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,9 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = LazyVim.safe_keymap_set
-
-map("n", "<leader>t", "", { desc = "Terminals" })
-map("n", "<leader>ts", "<cmd>TermSelect<CR>", { desc = "TermSelect" })
-map("n", "<leader>tp", "<cmd>PythonTerminal<CR>", { desc = "PythonTerminal" })
-map("n", "<leader>tn", "<cmd>Terminal<CR>", { desc = "Terminal" })
+-- local map = LazyVim.safe_keymap_set
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,5 +2,9 @@ return {
   {
     "akinsho/toggleterm.nvim",
     config = true,
+    init = function()
+      require("plugins.toggleterm.commands")
+      require("plugins.toggleterm.keymaps")
+    end,
   },
 }
