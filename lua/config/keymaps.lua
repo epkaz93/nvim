00157abff26659210ed26bd16e8253e0a626aeb9
epkaz93 <thomaswilmot@gmail.com>
2024-10-05 16:05:39 +1000

    Initial implementation of custom terminals

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+map("n", "<leader>t", "", { desc = "Terminals" })
+map("n", "<leader>ts", "<cmd>TermSelect<CR>", { desc = "TermSelect" })
+map("n", "<leader>tp", "<cmd>PythonTerminal<CR>", { desc = "PythonTerminal" })
+map("n", "<leader>tn", "<cmd>Terminal<CR>", { desc = "Terminal" })
