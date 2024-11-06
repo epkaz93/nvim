10bcfa7b033e047ad002442c35a8af024b99d781
epkaz93 <thomaswilmot@gmail.com>
2024-11-06 12:46:07 +1100

    Handle keymaps as god intended

--- lua/plugins/toggleterm/init.lua
+++ lua/plugins/toggleterm/init.lua
@@ -2,9 +2,21 @@ return {
   {
     "akinsho/toggleterm.nvim",
     config = true,
+    keys = {
+      { "<leader>ts", "<cmd>TermSelect<CR>", desc = "Select Terminal" },
+      { "<leader>tp", "<cmd>PythonTerminal<CR>", desc = "Python Terminal" },
+      { "<leader>tt", "<cmd>Terminal<CR>", desc = "Shell Terminal" },
+    },
     init = function()
       require("plugins.toggleterm.commands")
-      require("plugins.toggleterm.keymaps")
+      -- require("plugins.toggleterm.keymaps")
+
+      local wk = require("which-key")
+      wk.add({
+        { "<leader>t", group = "Terminals" },
+        { "<leader>tp", icon = { icon = "", color = "blue" } },
+        { "<leader>ts", icon = { icon = "", color = "green" } },
+      })
     end,
   },
 }
--- lua/plugins/toggleterm/keymaps.lua
+++
@@ -1,6 +0,0 @@
-local map = LazyVim.safe_keymap_set
-
-map("n", "<leader>t", "", { desc = "Terminals" })
-map("n", "<leader>ts", "<cmd>TermSelect<CR>", { desc = "TermSelect" })
-map("n", "<leader>tp", "<cmd>PythonTerminal<CR>", { desc = "PythonTerminal" })
-map("n", "<leader>tn", "<cmd>Terminal<CR>", { desc = "Terminal" })
